a5e701e423e63b267671dc1696cad430dfedd7a3
Nastya121 <77975134+Nastya121@users.noreply.github.com>
2021-03-07 23:09:21 +0400

    feat : add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (n) 
+ {
+   if (n<0){n*=-1;}
+  let s=String(n);   
+  let sh = s.split('');
+  let p=sh.reverse();
+  let j=p.join('');
+return j;
 }
